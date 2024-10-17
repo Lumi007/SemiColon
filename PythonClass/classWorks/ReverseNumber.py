86a75d480ac8d9db1286c6a39da242472d6f925a
0d-fako <od.fakorede@gmail.com>
2024-10-17 17:33:08 +0100

    Odd And Even Counter implemented

--- PythonClass/classWorks/ReverseNumber.py
+++ PythonClass/classWorks/ReverseNumber.py
@@ -2,4 +2,17 @@ number = int(input("Enter a number between 100 and 999: "))
 first_digit = number // 100
 second_digit = (number // 10) % 10
 third_digit = number % 10
-print(third_digit, second_digit, first_digit)
+odd_counter = 0
+even_counter = 0
+
+if (first_digit % 2 == 0) : even_counter+= 1
+else : odd_counter+= 1
+
+if (second_digit % 2 == 0) : even_counter+= 1  
+else : odd_counter+= 1
+
+if (third_digit % 2 == 0) : even_counter+= 1 
+else : odd_counter+= 1
+
+print("Number of odd Number", odd_counter)
+print("Number of even Number", even_counter)
