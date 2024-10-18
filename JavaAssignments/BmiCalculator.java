9bf87f69be8108214cff265ef49bf17e065351d0
0d-fako <od.fakorede@gmail.com>
2024-10-18 10:17:03 +0100

    Java Assignment Updated

--- JavaAssignments/BmiCalculator.java
+++ JavaAssignments/BmiCalculator.java
@@ -11,9 +11,19 @@ public class BmiCalculator {
 		System.out.print("Please enter your height in meters: ");
 		float heightInMeters = input.nextFloat();
 
-		double bmi = weightInKg/ (heightInMeters * heightInMeters);
+		double bmi = weightInKg / (heightInMeters * heightInMeters);
 
 		System.out.printf("Your BMI is: %.2f%n", bmi);
+
+		 if (bmi < 18.5) {
+            		System.out.printf("You are Under Weight");
+        	} else if (bmi < 25) {
+            		System.out.printf("You are Normal Weight");
+        	} else if (bmi < 30) {
+           		System.out.printf("You are Over Weight");
+        	} else {
+            		System.out.printf("You are Obese");
+        	}
 
 	}
 
