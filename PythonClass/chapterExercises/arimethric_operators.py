34251c373844f31aff81059e3b1b65afb20ec8e8
0d-fako <od.fakorede@gmail.com>
2024-10-23 10:36:23 +0100

    Assignment Updated

---
+++ PythonClass/chapterExercises/arimethric_operators.py
@@ -0,0 +1,13 @@
+
+left_operand = 27.5
+right_operand = 2
+
+
+print(f"For left_operand = {left_operand} and right_operand = {right_operand}:")
+print("-" * 50)
+print(f"Addition (+):\t\t{left_operand} + {right_operand} = {left_operand + right_operand}")
+print(f"Subtraction (-):\t\t{left_operand} - {right_operand} = {left_operand - right_operand}")
+print(f"Multiplication (*):\t{left_operand} * {right_operand} = {left_operand * right_operand}")
+print(f"Division (/):\t\t{left_operand} / {right_operand} = {left_operand / right_operand}")
+print(f"Floor Division (//):\t{left_operand} // {right_operand} = {left_operand // right_operand}")
+print(f"Exponentiation (**):\t{left_operand} ** {right_operand} = {left_operand ** right_operand}")
