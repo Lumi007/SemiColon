ae4f0b024d03e28dd01296ece7b02bcafac3cade
0d-fako <od.fakorede@gmail.com>
2024-10-28 02:58:34 +0100

    Assignments updated

---
+++ JavaAssignments/StarPatterns.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class StarPatterns {
+    public static void main(String[] args) {
+        System.out.println("Pattern A:");
+         for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("\nPattern B:");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+	}
+        
+        System.out.println("\nPattern C:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+	}
+        
+        System.out.println("\nPattern D:");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+	}
+        
+ 
+   
+    
+  
+    }
+}
