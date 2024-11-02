4f3a82c49f4ce60e2c690b98c9fb8066aee26402
0d-fako <od.fakorede@gmail.com>
2024-11-02 23:59:21 +0100

    Chapter Three

---
+++ PythonClass/chapterExercises/chapter_three/validate_input.py
@@ -0,0 +1,13 @@
+passes = 0
+valid_inputs = 0
+    
+while valid_inputs < 5:  # Get 5 valid inputs
+       value = int(input('Enter 1 or 2: '))
+        if value in [1, 2]:
+            valid_inputs += 1
+            if value == 1:
+                passes += 1
+    
+    failures = valid_inputs - passes
+    print(f'Number of passes: {passes}')
+    print(f'Number of failures: {failures}')
