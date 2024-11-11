5d1c26e93564d1452d073c2185cb98349462f222
0d-fako <od.fakorede@gmail.com>
2024-11-12 00:10:54 +0100

    New Tasks

--- JavaAssignments/MBTIPersonalityTest.java
+++ JavaAssignments/MBTIPersonalityTest.java
@@ -6,11 +6,16 @@ import java.util.Arrays;
 public class MBTIPersonalityTest {
 	public static void main (String[] args)
 	{
-		int[] array = new int[10];
+		final int ARRAY_LENGTH = 10;
+
+		int[] arrayNumbers = new int[ARRAY_LENGTH];
+
+		int [] arrayFilled = {32, 27, 64, 18,95,14, 90, 70, 60, 37};
 		System.out.printf("%s%8s%n","Index", "Value");
 
-		for (int counter = 0; counter < array.length; counter++)	{
-			System.out.printf("%5d%8d%n", counter, array[counter]);
+		for (int counter = 0; counter < arrayFilled.length; counter++)	
+		{
+			System.out.printf("%5d%8d%n", counter, arrayFilled[counter]);
 		}
 
 
