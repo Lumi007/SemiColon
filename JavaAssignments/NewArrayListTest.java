7baedac030094aeba16ca2a604c6ed8919e1adf2
0d-fako <od.fakorede@gmail.com>
2024-11-16 20:21:11 +0100

    ArrayList Test

--- JavaAssignments/NewArrayListTest.java
+++ JavaAssignments/NewArrayListTest.java
@@ -8,10 +8,16 @@ public class NewArrayListTest {
 
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 
+		numbers.add(5);
+		numbers.add(3);
+		numbers.add(1);
+		numbers.add(8);
+		numbers.add(10);
 
+		numbers.sort(Comparator.naturalOrder());
+		System.out.println(numbers.toString());
+				
 		
-
-
 
 	}
 
