c233a4e82733e1d6704d6c7177088115eba0e046
0d-fako <od.fakorede@gmail.com>
2024-11-16 20:44:41 +0100

    ArrayList Test

--- JavaAssignments/NewArrayListTest.java
+++ JavaAssignments/NewArrayListTest.java
@@ -8,23 +8,23 @@ public class NewArrayListTest {
 
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-		numbers.add(5);
+		numbers.add(10);
 		numbers.add(3);
 		numbers.add(1);
 		numbers.add(8);
-		numbers.add(10);
+		numbers.add(9);
 
 		numbers.sort(Comparator.naturalOrder());
 		System.out.println(numbers.toString());
 
 		numbers.forEach(number -> {
-			numbers.set(numbers[number], number*2
-			System.out.println(numbers);
+			numbers.set(numbers.indexOf(number), number*2);
+			
 
 		
 		});
 				
-		
+		System.out.println(numbers);
 
 	}
 
