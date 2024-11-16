3ff99cc1ae997b69f271831a50190eac05be1747
0d-fako <od.fakorede@gmail.com>
2024-11-16 20:14:55 +0100

    ArrayList Test

---
+++ JavaAssignments/NewArrayListTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
+
+public class NewArrayListTest {
+	public static void main(String... args){
+
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+
+		
+
+
+
+	}
+
+
+
+
+}
