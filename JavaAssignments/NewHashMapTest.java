5501782055dd2e4ba01f425e2aa5b1201e74fd87
0d-fako <od.fakorede@gmail.com>
2024-11-16 21:56:24 +0100

    New HashMap Test

--- JavaAssignments/NewHashMapTest.java
+++ JavaAssignments/NewHashMapTest.java
@@ -3,9 +3,15 @@ import java.util.HashMap;
 
 public class NewHashMapTest {
 	public static void main(String... args){
+		HashMap<String, Integer> examScores = new HashMap<String, Integer>();
 
 
+		examScores.put("Maths", 75);
+		examScores.put("Physics", 89);
+		examScores.put("Programming", 90);
 
+
+		System.out.println(examScores.toString());
 
 
 
