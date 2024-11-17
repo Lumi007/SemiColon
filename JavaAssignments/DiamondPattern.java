588cf20d3daf8baca65c80c1a7a844f8ee9100a0
0d-fako <od.fakorede@gmail.com>
2024-11-17 22:00:52 +0100

    Patterns

---
+++ JavaAssignments/DiamondPattern.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+public class DiamondPattern{
+	public static void main(String... arg){
+	
+
+		Scanner input = new Scanner(System.in);
+
+
+		System.out.print("Please enter a number of rows: ");
+
+		int rows = input.nextInt();
+
+
+		for (int row = 1; row <= rows; row++) {
+			for (int spaceColumn = row; spaceColumn <= rows; spaceColumn++) {
+				System.out.print(" ");
+            		}
+			for (int starColumn = 1; starColumn <= row; starColumn++) {					System.out.print("*");
+			}
+
+			for (int starColumn = 1; starColumn <= row; starColumn++) {					System.out.print("*");
+
+			}
+			System.out.println();
+		}
+		
+
+		for (int row = 1; row <= rows; row++) {
+			for (int spaceColumn = rows; spaceColumn >= row; spaceColumn++) {
+				System.out.print(" ");
+            		}
+			for (int starColumn = rows; starColumn >= 1; starColumn--) {					System.out.print("*");
+			}
+
+			for (int starColumn =rows; starColumn >= 1; starColumn--) {					System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+
+
+
+}
