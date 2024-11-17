f24a62d58f8b8e7fb50143c9e7ab7e5e4c6a0e98
0d-fako <od.fakorede@gmail.com>
2024-11-17 01:03:11 +0100

    OOP Concepts

--- JavaAssignments/User.java
+++ JavaAssignments/User.java
@@ -1,10 +1,16 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
 
 public class User {
 
 	public String name;
 	public LocalDate dateOfBirth;
+	public ArrayList<Book> books = new ArrayList<Book>();
+
+	public void borrow (Book book){
+		this.books.add(book);
+	}
 
 	public int age(){
 
