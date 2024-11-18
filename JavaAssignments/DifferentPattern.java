2df50f66fc54198e9fc5e2882fbeeef53121b591
0d-fako <od.fakorede@gmail.com>
2024-11-18 12:41:36 +0100

    Patterns

---
+++ JavaAssignments/DifferentPattern.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class DifferentPattern{
+	public static void main(String... arg){
+	
+
+		Scanner input = new Scanner(System.in);
+
+
+		System.out.print("Please enter a number of rows: ");
+
+		int rows = input.nextInt();
+
+
+		for (int row = 1; row <= rows; row++) {
+			for (int spaceColumn = row; spaceColumn <= rows; spaceColumn++) {
+				System.out.print("  ");
+            		}
+			for (int starColumn = 1; starColumn <= row; starColumn++) {					System.out.print("* ");
+			}
+
+			for (int starColumn = 1; starColumn <= row-1; starColumn++) {					System.out.print("* ");
+
+			}
+			System.out.println();
+		}
+		
+
+	
+	}
+
+
+
+
+}
