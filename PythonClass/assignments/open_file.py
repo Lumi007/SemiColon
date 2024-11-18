8524e593efd4f94bc8e61f71ee8e9e6e948f4eed
0d-fako <od.fakorede@gmail.com>
2024-11-18 18:26:18 +0100

    open a file

---
+++ PythonClass/assignments/open_file.py
@@ -0,0 +1,5 @@
+student_file = open("students.txt", "r" )
+
+print(student_file.read())
+
+student_file.close
--- PythonClass/assignments/try_except.py
+++ PythonClass/assignments/try_except.py
@@ -1,11 +1,11 @@
 try:
-	value = 10/0
-	number = int(input("Enter a number"))
+	#value = 10/0
+	number = int(input("Enter a number: "))
 	print(number)
 
 
-except ZeroDivisionError   
-	print("Divided by Zero")
+except ZeroDivisionError as err:  
+	print(err)
 
-except ValueError
-	print("Invalid input")
+except ValueError as err:
+	print("err")
