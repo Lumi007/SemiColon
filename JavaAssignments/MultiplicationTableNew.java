71d1a01693532bd246c181c3e3d589fa9e196ea1
0d-fako <od.fakorede@gmail.com>
2024-11-20 12:38:41 +0100

    New Java Tasks

---
+++ JavaAssignments/MultiplicationTableNew.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+public class MultiplicationTableNew {
+	public static void main(String... args){
+
+		for(int num = 1; num <= 10; num++){
+			for (int mult = 1; mult <= 12; mult++){
+			
+				System.out.printf("%2d *%2d = %2d\t", mult, num, num*mult);
+
+
+			}
+		System.out.println();
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+}
--- JavaAssignments/TicketSystem.java
+++ JavaAssignments/TicketSystem.java
@@ -5,7 +5,12 @@ public class TicketSystem {
 	public static void main(String... args){
 
 
+	Scanner input = new Scanner(System.in);
 
+
+
+	System.out.print("Hello, Welcome to the Semicolon Cinema, Please enter your name:  ");
+		String customerName = input.nextLine();
 
 
 
