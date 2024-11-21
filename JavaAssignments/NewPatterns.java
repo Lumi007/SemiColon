a9f2f66bd11a8af16f933cd4d14892ea741379a4
0d-fako <od.fakorede@gmail.com>
2024-11-21 10:14:16 +0100

    Butterfly Pattern

---
+++ JavaAssignments/NewPatterns.java
@@ -0,0 +1,47 @@
+public class NewPatterns {
+	public static void main(String... args){
+	
+
+		int rows = 9;
+		for (int row = 1; row <= rows; row++){
+			for (int star = 1; star <= row; star++){
+				System.out.print("*");
+			}
+			for (int space = row; space <= rows; space++){
+					System.out.print(" ");
+			}	
+			for (int space = row; space <= rows; space++){
+					System.out.print(" ");
+				
+			}
+			for (int star2 = 1; star2 <= row; star2++){
+					System.out.print("*");
+				
+			}
+			
+			
+		System.out.println();
+		}
+		for (int row = 1; row <= rows; row++){
+			for (int space = 1; space <= row; space++){
+				System.out.print("  ");
+			}
+			for (int star = row; star <= rows-1; star++){
+					System.out.print(" * ");
+				
+			}
+		System.out.println();
+		}
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
