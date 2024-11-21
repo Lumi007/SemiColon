a3c98efaa6f13e6ec7c349ff7058e315bf851fb4
0d-fako <od.fakorede@gmail.com>
2024-11-21 12:47:04 +0100

    Python Functions

--- PythonClass/classWorks/Tigernuts/dupchecker.py
+++ PythonClass/classWorks/Tigernuts/dupchecker.py
@@ -1,10 +1,11 @@
 def list_duplicate_checker(numbers:list):
-	for number1 in numbers:
-		for number2 in numbers:
-			if number == number:
+	for numberOne in range(len(numbers)):
+		for numberTwo in range(numberOne+1, len(numbers)):
+			if numberOne == numberTwo:
 				return True
 			else:
 				return False
 
 
 print(list_duplicate_checker([1,2,3,2,3]))
+print(list_duplicate_checker([1,2,3,4,5]))
