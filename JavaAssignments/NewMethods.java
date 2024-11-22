8bb7a20bcd8a077c54d8e1e38821f9dc52f69976
0d-fako <od.fakorede@gmail.com>
2024-11-22 23:57:25 +0100

    Java Methods

--- JavaAssignments/NewMethods.java
+++ JavaAssignments/NewMethods.java
@@ -13,7 +13,10 @@ public class NewMethods {
 	int numberOfTimes = input.nextInt();
 	printStar(numberOfTimes);
 	printStar(numberOfTimes);
+	newLine(6);
 	printStar(numberOfTimes);
+	printStar(numberOfTimes);
+	newLine();
 
 	
 	
@@ -29,7 +32,12 @@ public class NewMethods {
 
 	public static void newLine(int numberOfTimes){
 		for (int count = 0; count < numberOfTimes; count++)
-			System.out.println;
+			System.out.println();
+
+	}
+	
+	public static void newLine(){
+			System.out.println();
 
 	}
 
