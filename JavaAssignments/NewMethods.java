aea7368645b0a4fc650412d844d97774b1cb5646
0d-fako <od.fakorede@gmail.com>
2024-11-22 23:34:13 +0100

    Java Methods

--- JavaAssignments/NewMethods.java
+++ JavaAssignments/NewMethods.java
@@ -6,9 +6,16 @@ public class NewMethods {
 	public static void main(String... args){
 	
 	Scanner input = new Scanner(System.in);
+	
+
+	System.out.print("Please enter a number of rows: "):
+	
+	
 
 	public static String printStar(int numberOfTimes){
-	
+		for (int count = 0; count < numberOfTimes; count++
+			print("*");
+	}
 
 
 
