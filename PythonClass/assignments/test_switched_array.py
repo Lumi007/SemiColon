95d45da7e528b8c32818bf124f08297992b06f94
0d-fako <od.fakorede@gmail.com>
2024-11-25 13:15:26 +0100

    Arrays and Function

--- PythonClass/assignments/test_switched_array.py
+++ PythonClass/assignments/test_switched_array.py
@@ -1,15 +1,22 @@
 from unittest import TestCase
-from switchedarrays import *
+from switchedarray import *
 
 
 
 class TestSwitchedArraysFunction(TestCase):
-	def test_that_function_exists(self)
-	switched_array([1,2,3,4,5],3)
+	def test_that_function_exists(self):
+		switched_array([1,2,3,4,5],3)
 
 
 
 
 class TestSplitArraysFunction(TestCase):
-	def test_that_function_exists(self)
-	split_array([1,2,3,4,5],3)
+	def test_that_function_exists(self):
+		split_array([1,2,3,4,5,6])
+
+
+
+
+class TestSplitArraysFunction(TestCase):
+	def test_that_function_exists(self):
+		get_boolean_even_odd([1,2,3,4,5])
