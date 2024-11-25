428c8f3fb0bc578bab471693b8d4000a2bdd6eea
0d-fako <od.fakorede@gmail.com>
2024-11-25 13:10:08 +0100

    Arrays and Function

---
+++ PythonClass/assignments/test_switched_array.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from switchedarrays import *
+
+
+
+class TestSwitchedArraysFunction(TestCase):
+	def test_that_function_exists(self)
+	switched_array([1,2,3,4,5],3)
+
+
+
+
+class TestSplitArraysFunction(TestCase):
+	def test_that_function_exists(self)
+	split_array([1,2,3,4,5],3)
