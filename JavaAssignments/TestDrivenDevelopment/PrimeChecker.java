d33a72ab90c4e0ba84aec8ccbe9a6ab9acfc5eb8
0d-fako <od.fakorede@gmail.com>
2024-11-26 21:33:48 +0100

    Testing In Java

--- JavaAssignments/TestDrivenDevelopment/PrimeChecker.java
+++ JavaAssignments/TestDrivenDevelopment/PrimeChecker.java
@@ -1,25 +1,15 @@
-import static java.util.stream.IntStream.rangeClosed;
+import static java.util.stream.IntStream.range;
 import java.util.stream.IntStream;
 
+public class PrimeChecker {
+    public int[] getPrimeNumberBeforeInput(int input) {
+        return IntStream.range(2, input)
+            .filter(this::isNumberPrime)
+            .toArray();
+    }
 
-public class PrimeChecker{
-	public int[] getPrimeNumberBeforeInput(int input){
-		return IntStream.rangeClosed(2, input)
-			.filter((number) ->isNumberPrime(number))
-			.toArray();
-
-
-
-
-	}
-
-
-	private boolean isNumberPrime(int number){
-		return IntStream.rangeClosed(1, number)
-			.filter((x) -> x % number == 0)
-			.toArray().length == 0;
-
-		
-	}
-
+    private boolean isNumberPrime(int number) {
+        return IntStream.rangeClosed(2, (int)Math.sqrt(number))
+            .noneMatch(divisor -> number % divisor == 0);
+    }
 }
--- JavaAssignments/TestDrivenDevelopment/PrimeCheckerStandardTest.java
+++ JavaAssignments/TestDrivenDevelopment/PrimeCheckerStandardTest.java
@@ -2,31 +2,19 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-
-public class PrimeCheckerStandardTest{
-
-	@Test
-	public static void getPrimeBeforeInputTest(){
-
-		//given (pre-conditions)
-		PrimeChecker primeChecker = new PrimeChecker();
-
-
-		//action (the functionality being tested)
-		int input = 19;
-		int[] primeNumbers = primeChecker.getPrimeNumberBeforeInput(input);
-		System.out.println("actual : " + Arrays.toString(primeNumbers));
-
-
-		//check
-		int[] expected = {2,3,5,7,11,13,17};
-		assertArrayEquals(expected, primeNumbers);	
-
-		
-	}
-
-
-
-
-
+public class PrimeCheckerStandardTest {
+    @Test
+    public void getPrimeBeforeInputTest() {
+        // given (pre-conditions)
+        PrimeChecker primeChecker = new PrimeChecker();
+        
+        // action (the functionality being tested)
+        int input = 19;
+        int[] primeNumbers = primeChecker.getPrimeNumberBeforeInput(input);
+        System.out.println("actual : " + Arrays.toString(primeNumbers));
+        
+        // check
+        int[] expected = {2, 3, 5, 7, 11, 13, 17};
+        assertArrayEquals(expected, primeNumbers);    
+    }
 }
