498f1ecf65a1b996d7ceccab05e532c676712310
0d-fako <od.fakorede@gmail.com>
2024-11-26 17:03:32 +0100

    Testing In Java

---
+++ JavaAssignments/TestDrivenDevelopment/PrimeCheckerTest.java
@@ -0,0 +1,31 @@
+public class PrimeCheckerTest{
+	public void getPrimeBeforeInputTest(){
+
+		//given (pre-conditions)
+		PrimeChecker primeChecker = new PrimeChecker
+
+
+		//action (the functionality being tested)
+		int input = 19
+		int[] primeNumbers = primeChecker.getPrimeNumbersBeforeInput(input)
+
+
+		//check
+		int[] expected = 
+		if ()
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
