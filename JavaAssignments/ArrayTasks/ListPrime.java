2e0d29b944eb2e0763c57afcb187cdb56f289bd5
0d-fako <od.fakorede@gmail.com>
2024-11-26 10:01:43 +0100

    Array Tasks

---
+++ JavaAssignments/ArrayTasks/ListPrime.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ListPrime {
+	public static void main(String... args){
+
+	Scanner input = new Scanner(System.in);
+
+
+
+	System.out.print("Please enter a number: ")
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+	public static int[] getPrime(number){
+		for (int i = 1; i <= number; i++){
+
+
+
+		}
+	
+	}	
+
+
+
+
+}
