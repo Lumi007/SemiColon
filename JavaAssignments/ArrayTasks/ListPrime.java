981ee73ccded8026fe8d31b2e169d85cee5103ac
0d-fako <od.fakorede@gmail.com>
2024-11-26 12:42:50 +0100

    Array Tasks

--- JavaAssignments/ArrayTasks/ListPrime.java
+++ JavaAssignments/ArrayTasks/ListPrime.java
@@ -2,45 +2,30 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ListPrime {
-	public static void main(String... args){
-
-	Scanner input = new Scanner(System.in);
-	ArrayList<Integer> prime = new ArrayList<Integer>();
-
-
-
-	System.out.print("Please enter a number: ");
-	int number = input.nextInt();
-
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-	}
-
-
-	public static int[] getPrime(int number){
-		for (int i = 1; i <= number; i++){
-			for (int j = 1; j < i; j++){
-				if (i % j = 0){
-				
-				}
-			}
-
-		}
-	
-	}	
-
-
-
-
+    public static ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+    
+    public static void main(String... args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter a number: ");
+        int number = input.nextInt();
+        System.out.print(getPrimeList(number));
+    }
+    
+    public static ArrayList<Integer> getPrimeList(int number) {
+              
+        for (int i = 1; i <= number; i++) {
+            int divisorCount = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    divisorCount++;
+                }
+            }
+            if (divisorCount == 2) {
+                primeNumbers.add(i);
+            }
+        }
+        
+        return primeNumbers;
+    }
 }
