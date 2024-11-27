ecd16f22752af4596cedc64c7a4b33ee0da7217a
0d-fako <od.fakorede@gmail.com>
2024-11-27 10:40:00 +0100

    Testing In Java

--- JavaAssignments/TestDrivenDevelopment/PrimeCheckerStandardTest.java
+++ JavaAssignments/TestDrivenDevelopment/PrimeCheckerStandardTest.java
@@ -3,18 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class PrimeCheckerStandardTest {
-    @Test
-    public void getPrimeBeforeInputTest() {
-        // given (pre-conditions)
-        PrimeChecker primeChecker = new PrimeChecker();
+	@Test
+	public void getPrimeBeforeInputTest() {
+		// given (pre-conditions)
+		PrimeChecker primeChecker = new PrimeChecker();
         
-        // action (the functionality being tested)
-        int input = 19;
-        int[] primeNumbers = primeChecker.getPrimeNumberBeforeInput(input);
-        System.out.println("actual : " + Arrays.toString(primeNumbers));
+		// action (the functionality being tested)
+		int input = 19;	
+		int[] primeNumbers = primeChecker.getPrimeNumberBeforeInput(input);
+		System.out.println("actual : " + Arrays.toString(primeNumbers));
         
-        // check
-        int[] expected = {2, 3, 5, 7, 11, 13, 17};
-        assertArrayEquals(expected, primeNumbers);    
-    }
+     		// check
+        	int[] expected = {2, 3, 5, 7, 11, 13, 17};
+        	assertArrayEquals(expected, primeNumbers);    
+    	}
+
+
+
+	@Test
+	public void testThatGetPrimeNumberFailsForNegativeValue{
+	
+		
+	}
+
+
+
+	
 }
