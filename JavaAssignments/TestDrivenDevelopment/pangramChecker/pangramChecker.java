8b1dc1b86260e20398f644302f49cc9fcd89d177
0d-fako <od.fakorede@gmail.com>
2024-11-27 13:19:15 +0100

    Pangram Checker

---
+++ JavaAssignments/TestDrivenDevelopment/pangramChecker/pangramChecker.java
@@ -0,0 +1,14 @@
+public class pangramChecker{
+	public static boolean isPangram(String s) {
+        	if (s.length() < 26) {
+            		return false;
+		}
+        	s = s.toLowerCase();
+        	for (char i = 'a'; i <= 'z'; i++) {
+            		if (s.indexOf(i) == -1) {
+                		return false;
+			}
+        	}
+        	return true;
+	}
+}
